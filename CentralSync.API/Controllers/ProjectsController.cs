534240153d3f5323d5b2ec46acaa38737f0565f3
ReumiDev <reumidev@gmail.com>
2026-08-04 06:03:53 +0300

    Add ProjectsController with basic create operation, and a create request DTO for it

---
+++ CentralSync.API/Controllers/ProjectsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CentralSync.API.Data;
+using CentralSync.API.Models.Domain;
+using CentralSync.API.Models.DTO;
+
+namespace CentralSync.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectsController : ControllerBase
+    {
+        private readonly CentralSyncDbContext _dbcontext;
+
+        public ProjectsController(CentralSyncDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+
+        [HttpPost]
+        public IActionResult AddProject([FromBody] CreateProjectRequestDto project)
+        {
+            var projectDomainModel = new Project()
+            {
+                Name = project.Name,
+                Description = project.Description,
+
+            };
+
+            return Ok(projectDomainModel);
+        }
+    }
+}
---
+++ CentralSync.API/Models/DTO/CreateProjectRequestDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CentralSync.API.Models.DTO
+{
+    public class CreateProjectRequestDto
+    {
+        public string Name { get; set; }
+
+        public string? Description { get; set; }
+
+
+    }
+}
