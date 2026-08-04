dfae851610b6f44f6d5376101375695121f6583b
ReumiDev <reumidev@gmail.com>
2026-08-04 04:23:57 +0300

    Add EF Core packages, initialize DBContext class (DI, DBSets, Fluent API constraints), and initial migration

---
+++ CentralSync.API/Data/CentralSyncDbContext.cs
@@ -0,0 +1,51 @@
+﻿using CentralSync.API.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CentralSync.API.Data
+{
+    public class CentralSyncDbContext: DbContext
+    {
+        public CentralSyncDbContext(DbContextOptions<CentralSyncDbContext> options):base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<ProjectTask> Tasks { get; set; }
+        public DbSet<Comment> Comments {  get; set; }
+        public DbSet<ProjectMember> ProjectMembers { get; set; }
+        public DbSet<TaskHistory> TaskHistories { get; set; }
+        public DbSet<TaskTimeLog> TaskTimeLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasIndex(pm => new { pm.ProjectId, pm.UserId })
+                .IsUnique();
+
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            var cascadeFKs = modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetForeignKeys())
+                .Where(fk => !fk.IsOwnership && fk.DeleteBehavior == DeleteBehavior.Cascade);
+
+            foreach (var fk in cascadeFKs)
+            {
+                fk.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+
+            modelBuilder.Entity<ProjectTask>()
+                .Property(t => t.EstimatedHours)
+                .HasPrecision(18, 2);
+
+            modelBuilder.Entity<TaskTimeLog>()
+                .Property(t => t.Hours)
+                .HasPrecision(18, 2);
+        }
+
+    }
+}
