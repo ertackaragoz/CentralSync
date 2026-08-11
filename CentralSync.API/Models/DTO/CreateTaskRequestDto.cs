3e3054f00d245be32b6252b34088fc2e5c687543
ReumiDev <reumidev@gmail.com>
2026-08-11 03:02:06 +0300

    Feat: Add TaskDto and CreateTaskRequestDto

---
+++ CentralSync.API/Models/DTO/CreateTaskRequestDto.cs
@@ -0,0 +1,22 @@
+﻿using CentralSync.API.Models.Domain.Enums;
+
+namespace CentralSync.API.Models.DTO
+{
+    public class CreateTaskRequestDto
+    {
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public Guid? AssignedToUserId { get; set; }
+
+        public ProjectTaskPriority Priority { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public decimal? EstimatedHours { get; set; }
+
+    }
+}
---
+++ CentralSync.API/Models/DTO/TaskDto.cs
@@ -0,0 +1,36 @@
+﻿using CentralSync.API.Models.Domain;
+using CentralSync.API.Models.Domain.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CentralSync.API.Models.DTO
+{
+    public class TaskDto
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+
+        public Guid ProjectId { get; set; }
+
+        public Guid? AssignedToUserId { get; set; }
+
+        public Guid CreatedByUserId { get; set; }
+
+        public ProjectTaskStatus Status { get; set; }
+
+        public ProjectTaskPriority Priority { get; set; }
+
+        public DateTime? DueDate { get; set; }
+
+        public decimal? EstimatedHours { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime? UpdatedAt { get; set; }
+
+        public DateTime? CompletedAt { get; set; }
+    }
+}
