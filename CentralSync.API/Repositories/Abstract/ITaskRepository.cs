8a010669c78ada98265dbb21f4865ca530fba0bc
ReumiDev <reumidev@gmail.com>
2026-08-11 03:16:05 +0300

    Feat: Add and set up abstract and concrete repository layers for Tasks

---
+++ CentralSync.API/Repositories/Abstract/ITaskRepository.cs
@@ -0,0 +1,13 @@
+﻿using CentralSync.API.Models.Domain;
+using CentralSync.API.Models.Domain.Enums;
+
+namespace CentralSync.API.Repositories.Abstract
+{
+    public interface ITaskRepository
+    {
+        Task<IEnumerable<ProjectTask>> GetAllTasksAsync(int page=1, int pageSize = 10, Guid? projectId=null, ProjectTaskStatus? status = null, ProjectTaskPriority? priority = null);
+        Task<ProjectTask?> GetByIdAsync(Guid taskId);
+        Task<ProjectTask> CreateAsync(ProjectTask task);
+        Task<ProjectTask> UpdateAsync(ProjectTask task);
+    }
+}
---
+++ CentralSync.API/Repositories/Concrete/SQLTaskRepository.cs
@@ -0,0 +1,59 @@
+﻿using CentralSync.API.Data;
+using CentralSync.API.Models.Domain;
+using CentralSync.API.Models.Domain.Enums;
+using CentralSync.API.Repositories.Abstract;
+using Microsoft.EntityFrameworkCore;
+
+namespace CentralSync.API.Repositories.Concrete
+{
+    public class SQLTaskRepository : ITaskRepository
+    {
+        private readonly CentralSyncDbContext _dbcontext;
+
+        public SQLTaskRepository(CentralSyncDbContext dbcontext)
+        {
+            _dbcontext = dbcontext;
+        }
+        public async Task<ProjectTask> CreateAsync(ProjectTask task)
+        {
+            await _dbcontext.Tasks.AddAsync(task);
+            await _dbcontext.SaveChangesAsync();
+            return task;
+        }
+
+        public async Task<IEnumerable<ProjectTask>> GetAllTasksAsync(int page = 1, int pageSize = 10, Guid? projectId = null, ProjectTaskStatus? status = null, ProjectTaskPriority? priority = null)
+        {
+            var tasks = _dbcontext.Tasks.AsQueryable();
+
+            if (projectId.HasValue)
+            {
+                tasks = tasks.Where(x => x.Id == projectId.Value);
+            }
+
+            if (status.HasValue)
+            {
+                tasks = tasks.Where(x => x.Status == status.Value);
+            }
+
+            if (priority.HasValue)
+            {
+                tasks = tasks.Where(x => x.Priority == priority.Value);
+            }
+
+            var skipAmount = (page - 1) * pageSize;
+
+            return await tasks.Skip(skipAmount).Take(pageSize).ToListAsync();
+        }
+
+        public async Task<ProjectTask?> GetByIdAsync(Guid taskId)
+        {
+            return await _dbcontext.Tasks.FindAsync(taskId);
+        }
+
+        public async Task<ProjectTask> UpdateAsync(ProjectTask task)
+        {
+            await _dbcontext.SaveChangesAsync();
+            return task;
+        }
+    }
+}
