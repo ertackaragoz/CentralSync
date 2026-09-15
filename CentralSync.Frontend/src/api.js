0659bb2abfcf8ffd9183726019484a7358d64f8b
ReumiDev <reumidev@gmail.com>
2026-09-15 03:30:26 +0300

    fix(frontend): change baseURL to http and update port to 5265 in api.js to resolve SSL error

--- CentralSync.Frontend/src/api.js
+++ CentralSync.Frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://localhost:7079/api'
+    baseURL: 'http://localhost:5265/api'
 });
 
 api.interceptors.request.use((config) => {
